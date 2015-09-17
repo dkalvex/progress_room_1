69278f2e15c8f36a84f95532f86d0ea91a35f232
dkalvex <dkalvex@gmail.com>
2015-09-17 17:07:42 -0500

    tablas

---
+++ database/migrations/2015_09_17_164357_create_log_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log', function(Blueprint $table)
+        {
+            $table->increments('id')->unique();
+            $table->integer('id_event')->unsigned();
+            $table->integer('id_user')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::drop('log');
+    }
+}
