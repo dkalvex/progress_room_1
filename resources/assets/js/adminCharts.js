cbc8b504ed7a6575fa9c3ab8884597f044b59210
geovannyalonso <geovannyalonso@gmail.com>
2015-10-19 14:51:08 -0500

    Home Admin
    Actualización página principal del administrador

--- resources/assets/js/adminCharts.js
+++ resources/assets/js/adminCharts.js
@@ -1,55 +1,46 @@
-var topColors = ['#3C9787', '#CA7B5F', '#8964A2', '#CA595D', '#246F9A'];
+var topColors = ['#4cc3a3', '#4da1d0', '#a883b7', '#e4687c', '#ffc000', '#2f5597'];
+var timeline = [
+  {icon: 'fa fa-user', name: 'Jugador', action: ' ha ganado 20 ptos', detail: 'Nivel 2 del curso de Excel superado', style: 'primary'},
+  {icon: 'fa fa-user', name: 'Jugador', action: ' ha ganado 5 ptos', detail: 'Nivel 2 del curso de Excel superado', style: 'success'},
+  {icon: 'fa fa-user', name: 'Jugador', action: ' ha ganado 10 ptos', detail: 'Nivel 2 del curso de Excel superado', style: 'info'},
+  {icon: 'fa fa-user', name: 'Jugador', action: ' ha ganado 20 ptos', detail: 'Nivel 2 del curso de Excel superado', style: 'danger'},
+  {icon: 'fa fa-user', name: 'Jugador', action: ' ha ganado 25 ptos', detail: 'Nivel 2 del curso de Excel superado', style: 'info'},
+  {icon: 'fa fa-user', name: 'Jugador', action: ' ha ganado 20 ptos', detail: 'Nivel 2 del curso de Excel superado', style: 'success'},
+  {icon: 'fa fa-user', name: 'Jugador', action: ' ha ganado 20 ptos', detail: 'Nivel 2 del curso de Excel superado', style: 'warning'},
+  {icon: 'fa fa-user', name: 'Jugador', action: ' ha ganado 10 ptos', detail: 'Nivel 2 del curso de Excel superado', style: 'info'},
+  {icon: 'fa fa-user', name: 'Jugador', action: ' ha ganado 20 ptos', detail: 'Nivel 2 del curso de Excel superado', style: 'warning'},
+  {icon: 'fa fa-user', name: 'Jugador', action: ' ha ganado 10 ptos', detail: 'Nivel 2 del curso de Excel superado', style: 'danger'}
+];
 var path = '../../resources/assets/img/users/';
 
 $(function(){
   $.getScript('http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js',function(){
     $.getScript('http://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.0/morris.min.js',function(){
 
-      Morris.Line({
-        element: 'daily-players',
-        data: [
-          {day: 'Jue', value: 14},
-          {day: 'Vie', value: 23},
-          {day: 'Sab', value: 20},
-          {day: 'Dom', value: 12},
-          {day: 'Lun', value: 15},
-          {day: 'Ayer', value: 10},
-          {day: 'Hoy', value: 18}
-        ],
-        xkey: 'day',
-        ykeys: ['value'],
-        labels: ['Jugadores'],
-        lineColors: ['#ef4f69'],
-        parseTime: false
-      });
-      
       Morris.Donut({
-        element: 'active-players',
+        element: 'access-chart',
         data: [
-          {label: "Activos", value: 12},
-          {label: "Inactivos", value: 30},
+          {label: "6am - 9am", value: 40},
+          {label: "9am - 12pm", value: 25},
+          {label: "12pm - 3pm", value: 30},
+          {label: "3pm - 6pm", value: 19},
+          {label: "6pm - 9pm", value: 8},
         ],
-        colors: ['#4CC3A5', '#EFEFEF'],
+        colors: topColors,
         labelColor: '#666'
       });
 
       Morris.Donut({
-        element: 'active-courses',
+        element: 'league-chart',
         data: [
-          {label: "Activos", value: 10},
-          {label: "Inactivos", value: 4},
+          {label: "Liga 1", value: 80},
+          {label: "Liga 2", value: 51},
+          {label: "Liga 3", value: 26},
+          {label: "Liga 4", value: 19},
+          {label: "Liga 5", value: 11},
+          {label: "Liga 6", value: 4},
         ],
-        colors: ['#A883B9', '#EFEFEF'],
-        labelColor: '#666'
-      });
-
-      Morris.Donut({
-        element: 'active-missions',
-        data: [
-          {label: "Activos", value: 20},
-          {label: "Inactivos", value: 14},
-        ],
-        colors: ['#4DA1CF', '#EFEFEF'],
+        colors: topColors,
         labelColor: '#666'
       });
       
@@ -67,7 +58,7 @@ $(function(){
       Morris.Bar({
         element: 'top-courses',
         data: [
-          {y: 'Cursos', a: 20, b: 17, c: 12, d: 8, e: 5}
+          {y: 'Equipos', a: 90, b: 86, c: 74, d: 67, e: 65}
         ],
         xkey: 'y',
         ykeys: ['a', 'b', 'c', 'd', 'e'],
@@ -79,6 +70,8 @@ $(function(){
   
   showPlayersTable();
   showCoursesTable();
+  showStats();
+  showTimeline();
 });
 
 function showPlayersTable(){
@@ -101,7 +94,7 @@ function showPlayersTable(){
 
     var points = document.createElement('div');
     points.style.color = '#666';
-    points.innerHTML = i + ' ptos';
+    points.innerHTML = (10 - i) + ' ptos';
     points.style.fontSize = '10px';
 
     data.appendChild(name);
@@ -130,11 +123,11 @@ function showCoursesTable(){
 
     var name = document.createElement('div');
     name.style.color = topColors[i];
-    name.innerHTML = 'Curso ' + i;
+    name.innerHTML = 'Equipo ' + i;
 
     var points = document.createElement('div');
     points.style.color = '#666';
-    points.innerHTML = i + ' jugadores';
+    points.innerHTML = (10 - i) + ' ptos';
     points.style.fontSize = '10px';
 
     data.appendChild(name);
@@ -144,5 +137,79 @@ function showCoursesTable(){
     row.appendChild(data);
 
     $('#top-courses-table').append(row);
+  }
+}
+
+function showStats(){
+  var line_options = {
+    xaxis: {thickLength: 0, min: 0},
+    yaxis: {thickLength: 0, min: 0},
+    grid: {show: false, borderWidth: 0, hoverable: true},
+    series: {
+      lines: {show: true, fill: true},
+      points: {show: true, radius: 2, fillColor: '#ffffff'}
+    }
+  };
+
+  var green = [[1, 2153], [2, 2190], [3, 2476], [4, 2213], [5, 2126], [6, 2415], [7, 2350], [8, 2180], [9, 2500], [10, 2560]];
+  var data_green = [{data: green, color: '#3ea58a'}];
+  $.plot($('#green-stat'), data_green, line_options);
+
+  var blue = [[1, 1064], [2, 953], [3, 867], [4, 1159], [5, 1268], [6, 1026], [7, 1252], [8, 1541], [9, 1475], [10, 1354]];
+  var data_blue = [{data: blue, color: '#448db5'}];
+  $.plot($('#blue-stat'), data_blue, line_options);
+
+  var purple = [[1, 75], [2, 62], [3, 54], [4, 55], [5, 48], [6, 71], [7, 85], [8, 90], [9, 85], [10, 78]];
+  var data_purple = [{data: purple, color: '#9273a0'}];
+  $.plot($('#purple-stat'), data_purple, line_options);
+
+  var pink = [[1, 82], [2, 77], [3, 64], [4, 84], [5, 82], [6, 78], [7, 69], [8, 90], [9, 72], [10, 81]];
+  var data_pink = [{data: pink, color: '#c63b50'}];
+  $.plot($('#pink-stat'), data_pink, line_options);
+}
+
+function showTimeline(){
+  for(var i=0; i<timeline.length; i++){
+    var art = document.createElement('article');
+    art.className = 'timeline-entry'
+
+    var entry = document.createElement('div');
+    entry.className = 'timeline-entry-inner';
+
+    var badge = document.createElement('div');
+    badge.className = 'timeline-icon bg-' + timeline[i].style;
+
+    var icon = document.createElement('i');
+    icon.className = timeline[i].icon;
+
+    badge.appendChild(icon);
+
+    var label = document.createElement('div');
+    label.className = 'timeline-label';
+
+    var title = document.createElement('h2');
+
+    var name = document.createElement('a');
+    name.href = '#';
+    name.innerHTML = timeline[i].name;
+
+    var action = document.createElement('span');
+    action.innerHTML = timeline[i].action;
+
+    title.appendChild(name);
+    title.appendChild(action);
+
+    var text = document.createElement('p');
+    text.innerHTML = timeline[i].detail;
+
+    label.appendChild(title);
+    label.appendChild(text);
+
+    entry.appendChild(badge);
+    entry.appendChild(label);
+
+    art.appendChild(entry);
+
+    $('#timeline').append(art);
   }
 }
--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -3,77 +3,112 @@
 @section('content')
 <div class="container-fluid">
 	<div class="row">
-		<div class="page-title">
-			<i class="fa fa-home"></i> Inicio
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-xs-12">
-			<div class="panel panel-default">
-				<div class="panel-body">
-					<div class="chart-title">Total accesos únicos</div>
-					<div class="chart" id="daily-players"></div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-xs-12 col-md-4">
-			<div class="panel panel-default">
-				<div class="panel-body">
-					<div class="chart-title">Jugadores en Línea</div>
-					<div class="chart" id="active-players"></div>
-				</div>
-			</div>
-		</div>
-		<div class="col-xs-12 col-md-4">
-			<div class="panel panel-default">
-				<div class="panel-body">
-					<div class="chart-title">Cursos con Jugadores en Línea</div>
-					<div class="chart" id="active-courses"></div>
-				</div>
-			</div>
-		</div>
-		<div class="col-xs-12 col-md-4">
-			<div class="panel panel-default">
-				<div class="panel-body">
-					<div class="chart-title">Misiones con Jugadores en Línea</div>
-					<div class="chart" id="active-missions"></div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-xs-12 col-md-6">
-			<div class="panel panel-default">
-				<div class="panel-body">
-					<div class="chart-title">Top 5 Jugadores</div>
-					<div class="row">
-						<div class="col-xs-12 col-md-8">
-							<div class="chart top-chart" id="top-players"></div>
+		<div class="col-xs-12 col-sm-9">
+			<div class="row">
+				<div class="col-xs-6 col-sm-3">
+					<div class="stat green-stat">
+						<div class="title-stat">Puntos Distribuidos</div>
+						<div class="col-xs-4">
+							<div class="total-stat">2.560</div>
 						</div>
-						<div class="col-xs-12 col-md-4" id="top-players-table"></div>
+						<div class="col-xs-8">
+							<div class="chart-stat" id="green-stat"></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-sm-3">
+					<div class="stat blue-stat">
+						<div class="title-stat">Número de Visitas</div>
+						<div class="col-xs-4">
+							<div class="total-stat">1.354</div>
+						</div>
+						<div class="col-xs-8">
+							<div class="chart-stat" id="blue-stat"></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-sm-3">
+					<div class="stat purple-stat">
+						<div class="title-stat">Desempeño General</div>
+						<div class="col-xs-4">
+							<div class="total-stat">78%</div>
+						</div>
+						<div class="col-xs-8">
+							<div class="chart-stat" id="purple-stat"></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-sm-3">
+					<div class="stat pink-stat">
+						<div class="title-stat">Porcentaje Redenciones</div>
+						<div class="col-xs-4">
+							<div class="total-stat">81%</div>
+						</div>
+						<div class="col-xs-8">
+							<div class="chart-stat" id="pink-stat"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-6">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="chart-title">Distribución de acceso por hora</div>
+							<div class="chart" id="access-chart"></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-6">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="chart-title">Distribución de jugadores por liga</div>
+							<div class="chart" id="league-chart"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-6">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="chart-title">Top 5 Jugadores</div>
+							<div class="row">
+								<div class="col-xs-12">
+									<div class="chart top-chart" id="top-players"></div>
+								</div>
+								<div class="col-xs-12" id="top-players-table"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-6">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="chart-title">Top 5 Equipos</div>
+							<div class="row">
+								<div class="col-xs-12">
+									<div class="chart top-chart" id="top-courses"></div>
+								</div>
+								<div class="col-xs-12" id="top-courses-table"></div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="col-xs-12 col-md-6">
-			<div class="panel panel-default">
-				<div class="panel-body">
-					<div class="chart-title">Top 5 Cursos</div>
-					<div class="row">
-						<div class="col-xs-12 col-md-8">
-							<div class="chart top-chart" id="top-courses"></div>
-						</div>
-						<div class="col-xs-12 col-md-4" id="top-courses-table"></div>
-					</div>
-				</div>
+		<div class="col-xs-12 col-sm-3 timeline-container">
+			<div class="chart-title text-center">Actividad Reciente</div>
+			<div class="timeline-centered" id="timeline"></div>
+			<div class="text-center">
+				<button class="btn btn-primary">Ver todo</button>
 			</div>
 		</div>
 	</div>
 </div>
 @endsection
 @section('scripts')
+<script src="{{ asset('../resources/assets/js/flot/jquery.flot.min.js') }}"></script>
 <script src="{{ asset('../resources/assets/js/adminCharts.js') }}"></script>
 <script>
 	$("#li-home").addClass("active");
