925c4aef6d12fb23a2fae3fb5f4b2aa9dee72c32
geovannyalonso <geovannyalonso@gmail.com>
2015-10-20 09:33:49 -0500

    Actualización pantallas
    Se actualizan las páginas de visualización de todos los usuarios y todos
    los eventos

--- resources/assets/js/playerStats.js
+++ resources/assets/js/playerStats.js
@@ -18,19 +18,19 @@ $(function(){
     }
   };
 
-  var visits = [[1, 15], [2, 19], [3, 24], [4, 21], [5, 12], [6, 24], [7, 28], [8, 21], [9, 25], [10, 18]];
-  var data_visits = [{data: visits, color: '#3ea58a'}];
-  $.plot($('#access-stat'), data_visits, line_options);
+  var green = [[1, 15], [2, 19], [3, 24], [4, 21], [5, 12], [6, 24], [7, 28], [8, 21], [9, 25], [10, 18]];
+  var data_green = [{data: green, color: '#3ea58a'}];
+  $.plot($('#green-stat'), data_green, line_options);
 
-  var news = [[1, 6], [2, 5], [3, 8], [4, 1], [5, 2], [6, 0], [7, 2], [8, 5], [9, 4], [10, 2]];
-  var data_news = [{data: news, color: '#9273a0'}];
-  $.plot($('#new-stat'), data_news, line_options);
+  var blue = [[1, 6], [2, 5], [3, 8], [4, 1], [5, 2], [6, 1], [7, 2], [8, 5], [9, 4], [10, 5]];
+  var data_blue = [{data: blue, color: '#448db5'}];
+  $.plot($('#blue-stat'), data_blue, line_options);
 
-  var points = [[1, 850], [2, 1200], [3, 260], [4, 550], [5, 1200], [6, 2100], [7, 1850], [8, 1900], [9, 2010], [10, 1750]];
-  var data_points = [{data: points, color: '#c63b50'}];
-  $.plot($('#points-stat'), data_points, line_options);
+  var purple = [[1, 850], [2, 1200], [3, 260], [4, 550], [5, 1200], [6, 2100], [7, 1850], [8, 1900], [9, 2010], [10, 1750]];
+  var data_purple = [{data: purple, color: '#9273a0'}];
+  $.plot($('#purple-stat'), data_purple, line_options);
 
-  var redemptions = [[1, 2], [2, 7], [3, 4], [4, 4], [5, 2], [6, 7], [7, 6], [8, 10], [9, 2], [10, 6]];
-  var data_redemptions = [{data: redemptions, color: '#448db5'}];
-  $.plot($('#redemptions-stat'), data_redemptions, line_options);
+  var pink = [[1, 2], [2, 7], [3, 4], [4, 4], [5, 2], [6, 7], [7, 6], [8, 10], [9, 2], [10, 6]];
+  var data_pink = [{data: pink, color: '#c63b50'}];
+  $.plot($('#pink-stat'), data_pink, line_options);
 });
--- resources/views/users/all.blade.php
+++ resources/views/users/all.blade.php
@@ -4,77 +4,6 @@
 
 <div class="container-fluid">
 	<div class="row">
-		<div class="page-title">
-			<i class="fa fa-users"></i> Jugadores
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-xs-12 col-sm-6 col-md-3">
-			<div class="stat green-stat">
-				<div class="col-xs-3">
-					<div class="total-stat">18</div>
-					<div class="title-stat">Visitas</div>
-				</div>
-				<div class="col-xs-9">
-					<div class="chart-stat" id="access-stat"></div>
-				</div>
-				<div class="col-xs-12">
-					<div class="footer-stat">
-						<a href="/">Ver detalles</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-xs-12 col-sm-6 col-md-3">
-			<div class="stat purple-stat">
-				<div class="col-xs-3">
-					<div class="total-stat">2</div>
-					<div class="title-stat">Nuevos</div>
-				</div>
-				<div class="col-xs-9">
-					<div class="chart-stat" id="new-stat"></div>
-				</div>
-				<div class="col-xs-12">
-					<div class="footer-stat">
-						<a href="/">Ver detalles</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-xs-12 col-sm-6 col-md-3">
-			<div class="stat orange-stat">
-				<div class="col-xs-3">
-					<div class="total-stat">1750</div>
-					<div class="title-stat">Puntos</div>
-				</div>
-				<div class="col-xs-9">
-					<div class="chart-stat" id="points-stat"></div>
-				</div>
-				<div class="col-xs-12">
-					<div class="footer-stat">
-						<a href="/">Ver detalles</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-xs-12 col-sm-6 col-md-3">
-			<div class="stat blue-stat">
-				<div class="col-xs-3">
-					<div class="total-stat">6</div>
-					<div class="title-stat">Redenciones</div>
-				</div>
-				<div class="col-xs-9">
-					<div class="chart-stat" id="redemptions-stat"></div>
-				</div>
-				<div class="col-xs-12">
-					<div class="footer-stat">
-						<a href="/">Ver detalles</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
 		<div class="col-xs-12 col-md-6">
 			<div class="pager">
 				<button class="btn btn-default first"><i class="fa fa-angle-double-left"></i></button>
@@ -151,8 +80,6 @@
 </div>
 @endsection
 @section('scripts')
-<script src="{{ asset('../resources/assets/js/flot/jquery.flot.min.js') }}"></script>
-<script src="{{ asset('../resources/assets/js/playerStats.js') }}"></script>
 <script src="http://mottie.github.io/tablesorter/js/jquery.tablesorter.js"></script>
 <script src="http://mottie.github.io/tablesorter/js/jquery.tablesorter.widgets.js"></script>
 <script src="http://mottie.github.io/tablesorter/addons/pager/jquery.tablesorter.pager.js"></script>
