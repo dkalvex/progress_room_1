21be0fbf196418f5b55f9f23126f3fe730313589
geovannyalonso <geovannyalonso@gmail.com>
2015-10-28 11:39:19 -0500

    Actualización variables
    Actualización seeder para cargue de variables

--- database/seeds/VariablesTableSeeder.php
+++ database/seeds/VariablesTableSeeder.php
@@ -12,7 +12,11 @@ class VariablesTableSeeder extends Seeder
 			['id' => 2, 'name' => 'Eventos'],
 			['id' => 3, 'name' => 'Misiones'],
 			['id' => 4, 'name' => 'Torneos'],
-			['id' => 5, 'name' => 'Likes']
+			['id' => 5, 'name' => 'Likes'],
+			['id' => 6, 'name' => 'Logros'],
+			['id' => 7, 'name' => 'Perfil'],
+			['id' => 8, 'name' => 'Notas'],
+			['id' => 9, 'name' => 'Comentarios']
 		];
 		\DB::table('variables')->insert($variables);
 	}
